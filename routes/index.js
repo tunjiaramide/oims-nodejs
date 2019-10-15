da92c68f200931a6c58b33a5b1563d7280725055
Aramide Adetunji <aramideadetunji@Aramides-MacBook-Pro-2.local>
2019-10-15 13:00:01 +0100

    pagination

--- routes/index.js
+++ routes/index.js
@@ -8,26 +8,39 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-// List all Products
-router.get("/products", (req, res) => {
+// List all Products with Pagination
+
+router.get("/products/:page", (req, res) => {
+  var perPage = 20;
+  var page = req.params.page || 1;
+
   Cat.find({}).then(cats => {
     Product.find({})
-      .limit(300)
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec()
       .then(products => {
-        res.render("products", {
-          cats,
-          products
-        });
-      });
+        Product.countDocuments({})
+          .then(count => {
+            res.render("products", {
+              cats,
+              products,
+              current: page,
+              pages: Math.ceil(count / perPage)
+            });
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
   });
 });
 
 //List products by Category
-router.get("/products/:cat", (req, res) => {
-  let catName = req.params.cat;
+router.get("/cat/:id", (req, res) => {
+  let catName = req.params.id;
   Cat.find({}).then(cats => {
     Product.find({ category: catName }).then(products => {
-      res.render("products", {
+      res.render("products-cat", {
         cats,
         products
       });
@@ -95,6 +108,36 @@ router.get("/dashboard", (req, res) => {
   Product.countDocuments({}).then(c => {
     res.render("dashboard", {
       totalProducts: c
+    });
+  });
+});
+
+//Edit Product
+router.get("/product-detail/edit/:id", function(req, res, next) {
+  var productId = req.params.id;
+  Cat.find({}, function(err, cats) {
+    if (err) console.log(err);
+    Product.findOne({ _id: productId }, function(err, p) {
+      if (err) console.log(err);
+
+      res.render("edit-product", {
+        id: p._id,
+        productTitle: p.productTitle,
+        manDate: p.manDate,
+        expDate: p.expDate,
+        color: p.color,
+        imgUrl: p.imgUrl,
+        priceWholesale: p.priceWholesale,
+        priceRetail: p.priceRetail,
+        barCode: p.barCode,
+        sku: p.sku,
+        stockLevel: p.stockLevel,
+        size: p.size,
+        weight: p.weight,
+        category: p.category,
+        productDesc: p.productDesc,
+        categories: cats
+      });
     });
   });
 });
